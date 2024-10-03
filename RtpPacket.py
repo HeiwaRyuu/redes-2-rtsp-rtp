f1982f0b006b5c00a1fe11802c4acab17e572cb6
clib <vitor.carvalho.ufu@gmail.com>
2024-10-03 10:24:55 -0300

    Fixing Client and Server By adding encode function

--- RtpPacket.py
+++ RtpPacket.py
@@ -11,17 +11,25 @@ class RtpPacket:
 	def encode(self, version, padding, extension, cc, seqnum, marker, pt, ssrc, payload):
 		"""Encode the RTP packet with header fields and payload."""
 		timestamp = int(time())
-		header = bytearray(HEADER_SIZE)
-		#--------------
-		# TO COMPLETE
-		#--------------
+		self.header = bytearray(HEADER_SIZE)
+		
 		# Fill the header bytearray with RTP header fields
+		self.header[0] = version << 6 | padding << 5 | extension << 4 | cc
+		self.header[1] = marker << 7 | pt
+		self.header[2] = (seqnum >> 8) & 0xFF
+		self.header[3] = seqnum & 0xFF
+		self.header[4] = (timestamp >> 24) & 0xFF
+		self.header[5] = (timestamp >> 16) & 0xFF
+		self.header[6] = (timestamp >> 8) & 0xFF
+		self.header[7] = timestamp & 0xFF
+		self.header[8] = (ssrc >> 24) & 0xFF
+		self.header[9] = (ssrc >> 16) & 0xFF
+		self.header[10] = (ssrc >> 8) & 0xFF
+		self.header[11] = ssrc & 0xFF
 		
-		# header[0] = ...
-		# ...
-		
-		# Get the payload from the argument
-		# self.payload = ...
+		# Set the payload
+		self.payload = payload
+
 		
 	def decode(self, byteStream):
 		"""Decode the RTP packet."""
